1e8e250abfd11571546af3e77f84e651bd9ca8f9
hippiestank <hippiestank@chubbyskull.farm>
2024-06-28 12:27:06 -0600

    update weights, RuntimeOrigin

--- exercises/ex06-weights/weights/src/tests.rs
+++ exercises/ex06-weights/weights/src/tests.rs
@@ -1,53 +1,55 @@
 use crate as pallet_weights;
 use crate::mock::*;
-use frame_support::weights::{GetDispatchInfo, RuntimeDbWeight};
+use frame_support::weights::{ RuntimeDbWeight, Weight };
+use frame_support::dispatch::GetDispatchInfo;
+use sp_core::Get;
 
 #[test]
 fn verify_address_test() {
 	new_test_ext().execute_with(|| {
-		let db_weights: RuntimeDbWeight = <Test as frame_system::Config>::DbWeight::get();
-		let weight = pallet_weights::Call::<Test>::verify_address {}.get_dispatch_info().weight;
-		assert_eq!(weight, 10_000 + db_weights.reads(1));
+		let db_weights: RuntimeDbWeight = <TestRuntime as frame_system::Config>::DbWeight::get();
+		let weight = pallet_weights::Call::<TestRuntime>::verify_address {}.get_dispatch_info().weight;
+		assert_eq!(weight, Weight::from_parts(10_000, u64::MAX) + DbWeight::get().reads(1));
 	});
 }
 
 #[test]
 fn duplicate_test() {
 	new_test_ext().execute_with(|| {
-		let db_weights: RuntimeDbWeight = <Test as frame_system::Config>::DbWeight::get();
-		let weight1 = pallet_weights::Call::<Test>::duplicate_and_store { elem: 0, count: 1 }
+		let db_weights: RuntimeDbWeight = <TestRuntime as frame_system::Config>::DbWeight::get();
+		let weight1 = pallet_weights::Call::<TestRuntime>::duplicate_and_store { elem: 0, count: 1 }
 			.get_dispatch_info()
 			.weight;
-		let weight2 = pallet_weights::Call::<Test>::duplicate_and_store {
+		let weight2 = pallet_weights::Call::<TestRuntime>::duplicate_and_store {
 			elem: 0,
 			count: 1000,
 		}
 		.get_dispatch_info()
 		.weight;
 
-		assert!(weight1 < weight2);
-		assert!(weight1 > db_weights.writes(1));
+		assert!(weight1.proof_size() < weight2.proof_size());
+		assert!(weight1.proof_size() > db_weights.writes(1).proof_size());
 	});
 }
 
 #[test]
 fn store_maybe_hashed_test() {
 	new_test_ext().execute_with(|| {
-		let weight1 = pallet_weights::Call::<Test>::store_maybe_hashed {
+		let weight1 = pallet_weights::Call::<TestRuntime>::store_maybe_hashed {
 			data: vec![1, 2, 3],
 			hash: true,
 		}
 		.get_dispatch_info()
 		.weight;
-		let weight2 = pallet_weights::Call::<Test>::store_maybe_hashed {
+		let weight2 = pallet_weights::Call::<TestRuntime>::store_maybe_hashed {
 			data: vec![1, 2, 3],
 			hash: false,
 		}
 		.get_dispatch_info()
 		.weight;
 
-		assert_eq!(weight1, 100_000);
-		assert_eq!(weight2, 10_000);
+		assert_eq!(weight1, Weight::from_parts(100_000, u64::MAX));
+		assert_eq!(weight2, Weight::from_parts(10_000, u64::MAX));
 	});
 }
 
@@ -55,19 +57,19 @@ fn store_maybe_hashed_test() {
 fn benchmarked_store_maybe_hashed_test() {
 	new_test_ext().execute_with(|| {
 		let long_vec = vec![1; 100000];
-		let weight1 = pallet_weights::Call::<Test>::benchmarked_store_maybe_hashed {
+		let weight1 = pallet_weights::Call::<TestRuntime>::benchmarked_store_maybe_hashed {
 			data: long_vec.clone(),
 			hash: true,
 		}
 		.get_dispatch_info()
 		.weight;
-		let weight2 = pallet_weights::Call::<Test>::benchmarked_store_maybe_hashed {
+		let weight2 = pallet_weights::Call::<TestRuntime>::benchmarked_store_maybe_hashed {
 			data: long_vec,
 			hash: false,
 		}
 		.get_dispatch_info()
 		.weight;
 
-		assert!(weight1 > weight2);
+		assert!(weight1.proof_size() > weight2.proof_size());
 	});
 }
