675929092a0df42bf85cca6625add66e594b7bf3
hippiestank <hippiestank@chubbyskull.farm>
2024-06-28 12:26:38 -0600

    update dependencies

--- exercises/ex06-weights/weights/src/mock.rs
+++ exercises/ex06-weights/weights/src/mock.rs
@@ -1,23 +1,23 @@
 use crate as pallet_weights;
 use frame_support::{
 	derive_impl,
-    // parameter_types,
+    parameter_types,
 	traits::{ConstU16, ConstU64},
-    // weights::RuntimeDbWeight,
+    weights::RuntimeDbWeight,
 };
-// use frame_system as system;
+use frame_system as system;
 use sp_core::H256;
 use sp_runtime::{
 	traits::{BlakeTwo256, IdentityLookup},
 	BuildStorage,
 };
 
-//type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
-type Block = frame_system::mocking::MockBlock<Test>;
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<TestRuntime>;
+type Block = frame_system::mocking::MockBlock<TestRuntime>;
 
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
-	pub enum Test
+	pub enum TestRuntime
 	{
 		System: frame_system,
 		Weight: pallet_weights,
@@ -26,10 +26,11 @@ frame_support::construct_runtime!(
 );
 
 parameter_types! {
-    pub const DbWeight: RuntimeDBWeight = RuntimeDBWeight {read: 1, write: 100};
+    pub const DbWeight: RuntimeDbWeight = RuntimeDbWeight {read: 1, write: 100};
+}
 
 #[derive_impl(frame_system::config_preludes::TestDefaultConfig as frame_system::DefaultConfig)]
-impl frame_system::Config for Test {
+impl frame_system::Config for TestRuntime {
 	type BaseCallFilter = frame_support::traits::Everything;
 	type BlockWeights = ();
 	type BlockLength = ();
@@ -46,7 +47,7 @@ impl frame_system::Config for Test {
 	type BlockHashCount = ConstU64<250>;
 	type Version = ();
 	type PalletInfo = PalletInfo;
-	type AccountData = ();
+	type AccountData = pallet_balances::AccountData<u64>;
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
@@ -59,10 +60,11 @@ parameter_types! {
     pub const ExistentialDeposit: u64 = 1_000;
     pub const MaxLocks: u32 = 50;
     pub const MaxReserves: u32 = 50;
+    pub const MaxFreezes: u32 = 50;
 }
 
-impl pallet_balances::Config for Test {
-    type AccountStore = Sytem;
+impl pallet_balances::Config for TestRuntime {
+    type AccountStore = System;
     type Balance = u64;
     type DustRemoval = ();
     type RuntimeEvent = RuntimeEvent;
@@ -71,14 +73,18 @@ impl pallet_balances::Config for Test {
     type MaxReserves = MaxReserves;
     type ReserveIdentifier = [u8; 8];
     type WeightInfo = ();
+    type RuntimeHoldReason = RuntimeHoldReason;
+    type RuntimeFreezeReason = RuntimeFreezeReason;
+    type FreezeIdentifier = ();
+    type MaxFreezes = MaxFreezes;
 }
 
-impl pallet_weights::Config for Test {
+impl pallet_weights::Config for TestRuntime {
 	type RuntimeEvent = RuntimeEvent;
 	type WeightInfo = ();
 }
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-	frame_system::GenesisConfig::<Test>::default().build_storage().unwrap().into()
+	frame_system::GenesisConfig::<TestRuntime>::default().build_storage().unwrap().into()
 }
